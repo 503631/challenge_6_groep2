9d9231216a42448665606589158b191676b6a963
Stefan Bollen <503631@vistacollege.nl>
2021-02-15 14:12:11 +0100

    index start

---
+++ inc/ons_team.inc.php
@@ -0,0 +1,109 @@
+<style>
+#H8{
+    margin-top: 0px;
+}
+
+
+</style>
+
+
+
+<div class="bloks">
+<div class="bolks_despley">
+<img src="images/index.jpeg" alt="Car"  id="bloks_img">
+</div>
+<div class="bolks_text">
+<H8 style="font-size: 50%;"> naam</H8>
+<br>
+<h20 style="font-size: 30%;">hallo ik ben<h20>
+</div>
+</div>
+
+<div class="bloks">
+<div class="bolks_despley">
+<img src="images/index.jpeg" alt="Car"  id="bloks_img">
+</div>
+<div class="bolks_text">
+<H8 style="font-size: 50%;"> naam</H8>
+<br>
+<h20 style="font-size: 30%;">hallo<h20>
+</div>
+</div>
+
+<div class="bloks">
+<div class="bolks_despley">
+<img src="images/index.jpeg" alt="Car"  id="bloks_img">
+</div>
+<div class="bolks_text">
+<H8 style="font-size: 50%;"> naam</H8>
+<br>
+<h20 style="font-size: 30%;">hallo<h20>
+</div>
+</div>
+
+<div class="bloks">
+<div class="bolks_despley">
+<img src="images/index.jpeg" alt="Car"  id="bloks_img">
+</div>
+<div class="bolks_text">
+<H8 style="font-size: 50%;"> naam</H8>
+<br>
+<h20 style="font-size: 30%;">hallo<h20>
+</div>
+</div>
+
+<div class="bloks">
+<div class="bolks_despley">
+<img src="images/index.jpeg" alt="Car"  id="bloks_img">
+</div>
+<div class="bolks_text">
+<H8 style="font-size: 50%;"> naam</H8>
+<br>
+<h20 style="font-size: 30%;">hallo<h20>
+</div>
+</div>
+
+<div class="bloks">
+<div class="bolks_despley">
+<img src="images/index.jpeg" alt="Car"  id="bloks_img">
+</div>
+<div class="bolks_text">
+<H8 style="font-size: 50%;"> naam</H8>
+<br>
+<h20 style="font-size: 30%;">hallo<h20>
+</div>
+</div>
+
+<div class="bloks">
+<div class="bolks_despley">
+<img src="images/index.jpeg" alt="Car"  id="bloks_img">
+</div>
+<div class="bolks_text">
+<H8 style="font-size: 50%;"> naam</H8>
+<br>
+<h20 style="font-size: 30%;">hallo<h20>
+</div>
+</div>
+
+<div class="bloks">
+<div class="bolks_despley">
+<img src="images/index.jpeg" alt="Car"  id="bloks_img">
+</div>
+<div class="bolks_text">
+<H8 style="font-size: 50%;"> naam</H8>
+<br>
+<h20 style="font-size: 30%;">hallo<h20>
+</div>
+</div>
+
+<div class="bloks">
+<div class="bolks_despley">
+<img src="images/index.jpeg" alt="Car"  id="bloks_img">
+</div>
+<div class="bolks_text">
+<H8 style="font-size: 50%;"> naam</H8>
+<br>
+<h20 style="font-size: 30%;">hallo<h20>
+</div>
+</div>
+
--- index.php
+++ index.php
@@ -1,1 +1,54 @@
-oooo
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script src="https://kit.fontawesome.com/6488e6347e.js" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="css/style.css" />
+  <title>v!st@CARS</title>
+</head>
+
+<body>
+  <img class="logovista" src="images/logovista.webp" alt="">
+  <!-- #region Navbar-->
+  <div id="navbar">
+    <script>
+      $("#navbar").load("common/navbar.html");
+    </script>
+  </div>
+  <!-- #endregion -->
+
+  <div class="container">
+  <img src="images/index.jpeg" alt="Car" id="home_img">
+  <div class="top_img">V!ST@CARS</div>
+  <div class="centered">lorem ipsum is slechts een proefdruck</div>
+  </div>
+
+  <div class="bolk">
+    <div class="bolk_text">
+    <div class="bolk_text_text">
+        hallo
+    </div>  
+    </div>    
+  </div>
+    <div class="sowe_recauto">
+    recenten auto,s
+    <div class="sowe_recenteauto">
+nog bezig
+    </div>
+    <a href="showroom.php">bekijk showroom</a>
+    </div>
+    <div class="ons_team">
+<?php include_once("inc/ons_team.inc.php") ?>
+    </div>
+
+
+    <!-- #region Footer-->
+    <div id="footer">
+  <script>
+    $("#footer").load("common/footer.html");
+  </script>
+  </div>
+  <!-- #endregion -->
+</body>
+
+</html>
