af9cd4e940cb14acffc385527e3f5f09f8f732b7
Stefan Bollen <503631@vistacollege.nl>
2021-03-04 09:17:04 +0100

    contact pagina fix

--- admin/pages/weborders.php
+++ admin/pages/weborders.php
@@ -1,4 +1,5 @@
 <?php
+// include_once("./connection.inc.php");
 if (isset($_SESSION["ID"]) && ($_SESSION["STATUS"] != "ACTIEF")) {
     echo "<script>
             alert('U heeft geen toegang tot deze pagina.');
@@ -6,9 +7,44 @@ if (isset($_SESSION["ID"]) && ($_SESSION["STATUS"] != "ACTIEF")) {
         </script>";
 }
 ?>
+<?php
+$sql = "SELECT * FROM offertes" ;
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetchAll(); // get result
 
+foreach ($result as $key => $row)  {
+    $auto_id = $row['auto_id'];
+    $klant_id = $row['klant_id'];
+}
 
+    $sql = "SELECT * FROM autos_test where id = $auto_id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();   
+    $result = $stmt->fetchAll(); // get result
+    
+    foreach ($result as $key => $row)  {
+        $id = $row['id'];
+        $naam = $row['naam'];
+        // $img = $row['foto'];
+        $merken = $row['merken'];
+        $prijs = $row['prijs'];
+    }       
+    
+        $sql = "SELECT * FROM klant where id = $klant_id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll(); // get result
+        
+        foreach ($result as $key => $row)  {
+            $voornaam = $row['voornaam'];
+            $email = $row['email'];
 
+    }
+
+    
+
+    
 
 
 
